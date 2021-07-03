081857c4ef29b6e10b983e4f8568b539e929cba4
joelvillanuev <774804@clases.edu.sv>
2021-07-02 19:37:20 -0600

    Inicializando repositorio laravel

---
+++ resources/views/alumnos/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>create</h2>
+        </div>
+    </div>
+
+</div>
+
+@endsection
